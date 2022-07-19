fdf983355a478b80ec98f673a680bb0341baf293
Aelita <xsjctony@126.com>
2022-07-20 06:55:29 +1000

    Initialize ant-design-mobile

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,11 @@
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+import { Button } from 'antd-mobile'
+
+
+const App = (): JSX.Element => (
+  <div>
+    <Button color="primary">Button 1</Button>
+    <Button color="danger">Button 2</Button>
+  </div>
+)
 
 export default App

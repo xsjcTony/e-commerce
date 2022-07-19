b0308f564934b555c2bbad0d0c6e44a94c737426
Aelita <xsjctony@126.com>
2022-07-20 09:31:25 +1000

    Initialize postcss

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,4 +1,8 @@
 import react from '@vitejs/plugin-react'
+import autoprefixer from 'autoprefixer'
+import flexBugFixes from 'postcss-flexbugs-fixes'
+import pxToViewport from 'postcss-px-to-viewport'
+import writeSvg from 'postcss-write-svg'
 import { defineConfig } from 'vite'
 
 
@@ -6,5 +10,31 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true
+  },
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({}),
+        pxToViewport({
+          unitToConvert: 'px',
+          viewportUnit: 'vw',
+          fontViewportUnit: 'vw',
+          viewportWidth: 750,
+          viewportHeight: 1334,
+          unitPrecision: 3,
+          propList: ['*', '!letter-spacing'],
+          selectorBlackList: ['.ignore'],
+          minPixelValue: 1,
+          mediaQuery: true,
+          replace: true,
+          landscape: false,
+          exclude: [/node_modules/]
+        }),
+        flexBugFixes(),
+        writeSvg({
+          utf8: false
+        })
+      ]
+    }
   }
 })

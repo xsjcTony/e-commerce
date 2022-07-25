5ee1185fa9a97c3e6b03f4be42870e810c15c70a
Aelita <xsjctony@126.com>
2022-07-25 17:19:15 +1000

    Fix: clear text in search bar when exiting search mode

--- Frontend/src/features/search/components/Header.tsx
+++ Frontend/src/features/search/components/Header.tsx
@@ -1,9 +1,10 @@
 import { Button, NavBar, SearchBar } from 'antd-mobile'
 import { LeftOutline } from 'antd-mobile-icons'
 import clsx from 'clsx'
-import { memo } from 'react'
+import { memo, useRef } from 'react'
 import { useSearch } from '@/features/search'
 import styles from './Header.module.scss'
+import type { SearchBarRef } from 'antd-mobile/es/components/search-bar'
 import type { ReactNode } from 'react'
 
 
@@ -20,9 +21,19 @@ interface HeaderProps {
  * Component
  */
 export const Header = ({ navLeft = void 0, navRight = void 0 }: HeaderProps): JSX.Element => {
+  const searchRef = useRef<SearchBarRef>(null)
+
   const { isSearching, startSearching, stopSearching } = useSearch()
 
-  const searchLeft = <LeftOutline className={styles.searchNavLeft} onClick={stopSearching} />
+  const searchLeft = (
+    <LeftOutline
+      className={styles.searchNavLeft}
+      onClick={() => {
+        stopSearching()
+        searchRef.current?.clear()
+      }}
+    />
+  )
   const searchRight = <Button color="primary" size="small">Search</Button>
 
 
@@ -34,6 +45,7 @@ export const Header = ({ navLeft = void 0, navRight = void 0 }: HeaderProps): JS
       right={isSearching ? searchRight : navRight}
     >
       <SearchBar
+        ref={searchRef}
         className={styles.searchBar}
         placeholder="Search for anything"
         onFocus={startSearching}
